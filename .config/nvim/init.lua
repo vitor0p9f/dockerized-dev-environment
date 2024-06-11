e53741f821eda5dc171f0846c6a4e07753ec4b73
vitor0p9f <dev.vitorpfontes002@gmail.com>
2024-06-11 13:37:12 +0000

    chore(Neovim): Add Neovim configuration
    - Configure the TAB key to use two spaces.
    - Configure the Neovim to show absolute numbers in normal mode and
    relative numbers in insert mode.
    - Set the leader key to the spacebar.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,5 @@
+require("neovim-config")
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
---
+++ .config/nvim/lua/neovim-config.lua
@@ -0,0 +1,37 @@
+local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
+
+-- OPTIONS --
+
+-- Appearance
+
+vim.wo.number = true
+
+-- Indentation
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.wo.wrap = true
+vim.wo.linebreak = true
+
+-- Keybinds
+
+vim.g.mapleader = " "
+
+-- FUNCTIONS --
+
+-- Toggle bewteen relative numbers in insert mode and absolute numbers in visual mode
+
+autocmd('InsertEnter', {
+  callback = function()
+    vim.wo.number = false
+    vim.wo.relativenumber = true
+  end
+})
+
+autocmd('InsertLeave', {
+  callback = function()
+    vim.wo.relativenumber = false
+    vim.wo.number = true
+  end
+})
