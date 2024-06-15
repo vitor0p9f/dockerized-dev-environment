83064848a0063e03a3cc722b9fbd80ce8a928eea
vitor0p9f <dev.vitorpfontes002@gmail.com>
2024-06-15 18:40:00 +0000

    revert: Remove NodeJS
    Revert the commit 12eafd3

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    	atuin init fish | source
+  # Commands to run in interactive sessions can go here
+  atuin init fish | source
 
 	alias ls="exa --all --icons --long"
 	alias cat="bat"
@@ -10,8 +10,8 @@ source ~/.asdf/asdf.fish # Add asdf to $PATH
 
 function setup
 	set_color brmagenta
-    	echo "Installing asdf..."
-    	set_color normal
+  echo "Installing asdf..."
+  set_color normal
 	
 	git clone https://github.com/asdf-vm/asdf.git /root/.asdf/ --branch v0.14.0
 
@@ -19,37 +19,28 @@ function setup
 
 	set_color brmagenta
 	echo "Generating symbolic link for completions..."
-    	set_color normal
+  set_color normal
 
 	mkdir -p ~/.config/fish/completions; and ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions
 
 	set_color brmagenta
-    	echo "Installing Rust..."
-    	set_color normal
+  echo "Installing Rust..."
+  set_color normal
 
-    	asdf plugin-add rust
-    	asdf plugin-update rust
-    	asdf install rust latest
-    	sleep 1
-    	asdf global rust latest
-
-	set_color brmagenta
-    	echo "Installing Ruby + YJIT..."
-    	set_color normal
-
-    	set RUBY_CONFIGURE_OPTS --enable-yjit
-    	asdf plugin-add ruby
-    	asdf plugin-update ruby
-    	asdf install ruby latest
-    	sleep 1
-    	asdf global ruby latest
+  asdf plugin-add rust
+  asdf plugin-update rust
+  asdf install rust latest
+  sleep 1
+  asdf global rust latest
 
 	set_color brmagenta
-    	echo "Installing NodeJS..."
-    	set_color normal
+  echo "Installing Ruby + YJIT..."
+  set_color normal
 
-	asdf plugin-add nodejs
-	asdf plugin-update nodejs
-	asdf install nodejs latest
-	asdf global nodejs latest
+  set RUBY_CONFIGURE_OPTS --enable-yjit
+  asdf plugin-add ruby
+  asdf plugin-update ruby
+  asdf install ruby latest
+  sleep 1
+  asdf global ruby latest
 end
