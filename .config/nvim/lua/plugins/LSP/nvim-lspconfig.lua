5282174caecfe49bc01ad291e5ca047ce8f453d5
vitor0p9f <dev.vitorpfontes002@gmail.com>
2024-06-17 22:44:50 +0000

    feat(Neovim): Add Typescript LSP

--- .config/nvim/lua/plugins/LSP/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/LSP/nvim-lspconfig.lua
@@ -7,5 +7,22 @@ return {
       cmd = { "/root/.local/share/nvim/mason/packages/elixir-ls/language_server.sh" },
       root_dir = vim.loop.cwd
     })
+
+    lspconfig.tsserver.setup{
+      init_options = {
+        plugins = {
+          {
+            name = "@vue/typescript-plugin",
+            location = "/usr/local/lib/node_modules/@vue/typescript-plugin",
+            languages = {"javascript", "typescript", "vue"},
+          },
+        },
+      },
+      filetypes = {
+        "javascript",
+        "typescript",
+        "vue",
+      },
+    }
   end
 }
