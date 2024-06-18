5c7e98824c55af75ba4018e29363a6078ae974b2
vitor0p9f <dev.vitorpfontes002@gmail.com>
2024-06-18 03:03:09 +0000

    feat(Neovim): Add and configure lua_ls LSP
    Enable automatic installation using mason-lspconfig plugin.

--- .config/nvim/lua/plugins/LSP/mason-lspconfig.lua
+++ .config/nvim/lua/plugins/LSP/mason-lspconfig.lua
@@ -2,7 +2,7 @@ return {
 	"williamboman/mason-lspconfig.nvim",
 	config = function()
 		require("mason-lspconfig").setup({
-			ensure_installed = { "elixirls","tsserver" },
+			ensure_installed = { "elixirls", "tsserver", "lua_ls" },
 			automatic_installation = true,
 		})
 	end,
--- .config/nvim/lua/plugins/LSP/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/LSP/nvim-lspconfig.lua
@@ -24,5 +24,30 @@ return {
         "vue",
       },
     }
+
+    lspconfig.lua_ls.setup {
+      on_init = function(client)
+        local path = client.workspace_folders[1].name
+        
+        if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
+          return
+        end
+
+        client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+          runtime = {
+            version = 'LuaJIT'
+          },
+          workspace = {
+            checkThirdParty = false,
+            library = {
+              vim.env.VIMRUNTIME
+            }
+          }
+        })
+      end,
+      settings = {
+        Lua = {}
+      }
+    }
   end
 }
