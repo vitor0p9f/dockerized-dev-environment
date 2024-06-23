98c5863d14f5972216730060a32087b56c10f80f
vitor0p9f <dev.vitorpfontes002@gmail.com>
2024-06-23 18:29:35 +0000

    feat(Neovim): add new shortcut
    Add a shortcut to the Which Key config file that formats the buffer.

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -1,49 +1,53 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-  end,
-  config = function()
-    local wk = require("which-key")
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	init = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 300
+	end,
+	config = function()
+		local wk = require("which-key")
 
-    wk.register({
-      ["<leader>"] ={
-        f = {
-          name = "Telescope",
-          f = {"<cmd>Telescope find_files<cr>","Find file"},
-          r = {"<cmd>Telescope oldfiles<cr>","Recent files"},
-          w = {"<cmd>Telescope live_grep<cr>","Find word"}
-        },
-        t = {
-          name = "Tabs",
-          f = {"<cmd>BufferPin<cr>","Toggle - Pin tab"},
-          x = {"<cmd>BufferClose<cr>","Close active tab"},
-          p = {"<cmd>BufferPrevious<cr>","Move to the previous tab"},
-          n = {"<cmd>BufferNext<cr>","Move to the next tab"},
-        },
-        n = {
-          name = "Neo-tree",
-          t = {"<cmd>Neotree toggle<cr>","Toggle"},
-          f = {"<cmd>Neotree focus<cr>","Focus"}
-        },
-        h = {
-          name = "Hunk",
-          r = {"<cmd>Gitsigns reset_hunk<cr>","Reset"},
-          p = {"<cmd>Gitsigns preview_hunk<cr>","Preview"},
-          b = {"<cmd>Gitsigns toggle_current_line_blame<cr>","Toggle line blame"}
-        },
-        l = {
-          name = "Lazygit",
-          r = {"<cmd>LazyGit<cr>","Run"}
-        },
-        d = {
-          name = "Diagnostics",
-				  t = { "<cmd>Trouble diagnostics toggle<cr>", "Toggle trouble" },
-				  b = { "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", "Show buffer diagnostics" },
-        }
-      }
-    })
-  end
+		wk.register({
+			["<leader>"] = {
+				f = {
+					name = "Telescope",
+					f = { "<cmd>Telescope find_files<cr>", "Find file" },
+					r = { "<cmd>Telescope oldfiles<cr>", "Recent files" },
+					w = { "<cmd>Telescope live_grep<cr>", "Find word" },
+				},
+				t = {
+					name = "Tabs",
+					f = { "<cmd>BufferPin<cr>", "Toggle - Pin tab" },
+					x = { "<cmd>BufferClose<cr>", "Close active tab" },
+					p = { "<cmd>BufferPrevious<cr>", "Move to the previous tab" },
+					n = { "<cmd>BufferNext<cr>", "Move to the next tab" },
+				},
+				n = {
+					name = "Neo-tree",
+					t = { "<cmd>Neotree toggle<cr>", "Toggle" },
+					f = { "<cmd>Neotree focus<cr>", "Focus" },
+				},
+				h = {
+					name = "Hunk",
+					r = { "<cmd>Gitsigns reset_hunk<cr>", "Reset" },
+					p = { "<cmd>Gitsigns preview_hunk<cr>", "Preview" },
+					b = { "<cmd>Gitsigns toggle_current_line_blame<cr>", "Toggle line blame" },
+				},
+				l = {
+					name = "Lazygit",
+					r = { "<cmd>LazyGit<cr>", "Run" },
+				},
+				d = {
+					name = "Diagnostics",
+					t = { "<cmd>Trouble diagnostics toggle<cr>", "Toggle trouble" },
+				},
+				b = {
+					name = "Buffer",
+					d = { "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", "Diagnostics" },
+					f = { vim.lsp.buf.format, "Format" },
+				},
+			},
+		})
+	end,
 }
