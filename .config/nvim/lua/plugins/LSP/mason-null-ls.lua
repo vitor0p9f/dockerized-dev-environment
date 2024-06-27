53b3a00f83d357837b4c7c37e8ebf8f020398f94
vitor0p9f <dev.vitorpfontes002@gmail.com>
2024-06-27 02:19:40 +0000

    feat(Neovim): add and configure the commitlint diagnostics using none-ls plugin
    Add commitlint source to the none-ls config file.
    
    Generate the commitlint config file.
    
    Ensure the installation of commitlint using the mason-null-ls plugin.
    
    Bind the commitlint config file between the host and the container.

--- .config/nvim/lua/plugins/LSP/mason-null-ls.lua
+++ .config/nvim/lua/plugins/LSP/mason-null-ls.lua
@@ -1,10 +1,10 @@
 return {
-  "jay-babu/mason-null-ls.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    require("mason-null-ls").setup({
-      ensure_installed = { "stylua", "prettier" },
-      automatic_installation = true
-    })
-  end
+	"jay-babu/mason-null-ls.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		require("mason-null-ls").setup({
+			ensure_installed = { "stylua", "prettier", "commitlint" },
+			automatic_installation = true,
+		})
+	end,
 }
--- .config/nvim/lua/plugins/LSP/none-ls.lua
+++ .config/nvim/lua/plugins/LSP/none-ls.lua
@@ -8,7 +8,11 @@ return {
         null_ls.builtins.formatting.mix,
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.formatting.stylua,
+        null_ls.builtins.diagnostics.commitlint.with({
+          prefer_local = "/root/.local/share/nvim/mason/bin/commitlint",
+          extra_args = { "--config", "/root/.local/share/nvim/mason/packages/commitlint/commitlint.config.js" },
+        }),
       },
     })
-  end
+  end,
 }
