99227b5182f9d9bbe2fe08ac28ac18b3f38f6272
vitor0p9f <dev.vitorpfontes002@gmail.com>
2024-06-29 14:27:30 +0000

    feat(Neovim): add surface formater using none-ls plugin

--- .config/nvim/lua/plugins/LSP/none-ls.lua
+++ .config/nvim/lua/plugins/LSP/none-ls.lua
@@ -1,19 +1,23 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
 
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.mix,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.diagnostics.commitlint.with({
-          prefer_local = "/root/.local/share/nvim/mason/bin/commitlint",
-          extra_args = { "--config", "/root/.local/share/nvim/mason/packages/commitlint/commitlint.config.js" },
-        }),
-        null_ls.builtins.diagnostics.hadolint
-      },
-    })
-  end,
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.mix,
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.diagnostics.commitlint.with({
+					prefer_local = "/root/.local/share/nvim/mason/bin/commitlint",
+					extra_args = {
+						"--config",
+						"/root/.local/share/nvim/mason/packages/commitlint/commitlint.config.js",
+					},
+				}),
+				null_ls.builtins.diagnostics.hadolint,
+				null_ls.builtins.formatting.surface,
+			},
+		})
+	end,
 }
