8dee133aaa0ce2ecd5edbfe98b308b810b6a7b7b
vitor0p9f <dev.vitorpfontes002@gmail.com>
2024-11-10 14:09:33 -0300

    build(Scripts): add the init.sh script
    Add the command to start.
    
    Add the command to build.
    
    Add the comment to stop.
    
    Add the command to delete the container and the image.

---
+++ init.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+repository_directory=~/Documents/Programming/dockerized-dev-environment
+
+if [ "$#" -eq 0 ]; then
+  if [ ! -e $repository_directory/.env ]; then
+    env_file_path=$repository_directory/.env
+
+    touch $env_file_path
+
+    echo "USER_ID=$(id -u)" >> $env_file_path
+    echo "USER_NAME=$(id -un)" >> $env_file_path
+    echo "XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR" >> $env_file_path
+    echo "DISPLAY=$DISPLAY" >> $env_file_path
+    echo "XAUTHORITY=$XAUTHORITY" >> $env_file_path
+  fi
+
+  sudo systemctl start docker
+
+  sudo docker compose --file $repository_directory/compose.yml up -d
+
+  sudo docker compose --file $repository_directory/compose.yml exec -it dockerized-dev-environment fish
+else
+  case $1 in
+    "build")
+      sudo systemctl start docker
+
+      sudo docker-compose --file $repository_directory/compose.yml up -d --build
+
+      sudo docker compose --file $repository_directory/compose.yml exec -it dockerized-dev-environment fish
+    ;;
+
+    "stop")
+      sudo systemctl start docker
+
+      sudo docker-compose --file $repository_directory/compose.yml stop
+    ;;
+
+    "remove")
+      sudo systemctl start docker
+
+      sudo docker-compose --file $repository_directory/compose.yml down -v
+
+      sudo docker rmi dockerized-dev-environment
+    ;;
+  esac
+fi
