842341c4c987fd1a41841bba95b1237bedfce27c
vitor0p9f <dev.vitorpfontes002@gmail.com>
2024-11-19 21:37:18 -0300

    feat(Emacs): add the LSP Bridge package
    Configure the default-shell.nix file to include all LSP Bridge dependencies when imported by other shell.nix files.

--- config/nix/default-shell.nix
+++ config/nix/default-shell.nix
@@ -1,6 +1,17 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
   buildInputs = [
+    pkgs.python3
+    (pkgs.python3.withPackages (ps: with ps; [
+      epc
+      orjson
+      sexpdata
+      six
+      setuptools
+      paramiko
+      rapidfuzz
+      watchdog
+    ]))
   ];
 
   shellHook = ''
